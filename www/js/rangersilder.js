a3ef13d72c4cafb8134cf79af4e17f10522d3643
Bjørn F. Nimvik <bjorn.nimvik@niva.no>
2024-04-16 15:29:13 +0200

    added 4 sliders for hydration, flour. starter, salt

--- www/js/rangersilder.js
+++ www/js/rangersilder.js
@@ -1,3 +1,16 @@
-function rangeslider(value) {
-    document.getElementById("rangeslidervalue").innerHTML = value; 
+function rangeslidervalue_hydration(value) {
+    document.getElementById("rangeslidervalue_hydration").innerHTML = value + "%"; 
+}
+function rangeslider_starter(value) {
+    document.getElementById("rangeslidervalue_starter").innerHTML = value + "g"; 
+}
+function rangeslidervalue_flour(value) {
+    document.getElementById("rangeslidervalue_flour").innerHTML = value + "g"; 
+}
+function rangeslider_salt(value) {
+    document.getElementById("rangeslidervalue_salt").innerHTML = value/10 + "%"; 
+}
+
+function getWater(flour_g, hydration_pcnt ){
+    return (flour_g * hydration_pcnt/100); 
 }
